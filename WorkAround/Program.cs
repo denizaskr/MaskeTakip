371f7c1baa4887c2b1b96f82d5d6f203b657d4ec
denizaskar05 <denizaskar05@gmail.com>
2023-08-03 02:12:26 +0300

    Add project files.

---
+++ WorkAround/Program.cs
@@ -0,0 +1,35 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace WorkAround
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            SelamVer("Deniz Aşkar ");
+            Person person1 =new Person();
+            person1.FirstName = "deniz";
+            person1.LastName = "aşkar";
+            person1.DateOfBirthYear = 1900;
+            person1.NationalIdentity = 1;
+            
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+        static void SelamVer(string isim="")
+        {
+            Console.WriteLine(""+ isim);
+        }
+    }
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+
+    }
+}
